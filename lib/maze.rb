447d2c069dab7671a17fb3d37657115c806e89b9
Kevin <64184097+Kmullen444@users.noreply.github.com>
2021-11-17 15:29:13 -0800

    Added minitest gem for testing

--- lib/maze.rb
+++ lib/maze.rb
@@ -1,6 +1,6 @@
 class Maze
   def self.from_file(file)
-    maze = File.readlines(file).map do |line|
+    File.readlines(file).map do |line|
       [line.chomp.split("")]
     end
   end
@@ -9,5 +9,15 @@ class Maze
     @maze        = Maze.from_file(file)
     @start_point = []
     @end_point   = []
+  end
+
+  def find_start
+    start_indices = []
+    @maze.each_with_index do |line, i|
+      line.select.with_index do |ele, idx2|
+        start_indices << [i][idx2] if ele == "S"
+      end
+    end
+    start_indices
   end
 end
